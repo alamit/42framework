9dce2ed102dd56ca36dda869e697d24325e349b8
Adrien Lamit <alamit@e1z1r5p37.42.us.org>
2018-09-22 15:09:09 -0700

    initialization script

---
+++ init42.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+NB_EX=$1
+REPO_URL=$2
+BASEDIR=$3
+i=0
+
+clone=`git clone $REPO_URL $BASEDIR`
+
+if [ "$clone" -neq "*fatal: *"]
+then
+	echo "Failed to clone git repo. Exiting"
+else
+	echo "Git repo cloned successfully."
+fi
+
+cd $BASEDIR
+
+echo "*/" > .gitignore
+mkdir tests
+
+while [ $i -lt $NB_EX ]
+do
+	
+	if [ $i -lt 10 ]
+	then
+		dir="ex0$i"
+	else
+		dir="ex$i"
+	fi
+
+	mkdir $dir
+	echo "#include <libc.h>\n#include <stdio.h>\n#include <stdlib.h>" > tests/test_$dir.c
+
+	echo "Enter submission files for $dir separated by commas: \n"
+
+	read subs
+	IFS=', ' read -r -a sub_files <<< "$subs"
+	for filename in "${sub_files[@]}"
+	do
+		touch $dir/$filename
+		echo "!$dir/$filename" >> .gitignore
+		echo '#include "../'$dir'/'$filename'"' >> tests/test_$dir.c
+	done
+	echo "\nint\tmain(void)\n{\n/* Write your tests here and return 1 if something is wrong */\nreturn (0);\n}" >> tests/test_$dir.c
+	i=$(($i + 1))
+done
+
+exit
+
