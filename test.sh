017d4f1c4cffa564c594593c4a96f3279ada83c0
Adrien Lamit <alamit@e1z1r5p37.42.us.org>
2018-09-22 16:38:30 -0700

    fixed file exists  config

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ -f .42framework ]
+if [ ! -f .42framework ]
 then
-	echo "You can only execute this script in a repo initialized with the `init42` command."
+	echo "You can only execute this script in a repo initialized with the 'init42' command."
 	exit
 fi
 
