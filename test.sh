1d5ef6e15793a448b60813c10617ad14adbe7c44
Adrien Lamit <alamit@e1z1r5p37.42.us.org>
2018-09-24 14:31:47 -0700

    Add colors

--- test.sh
+++ test.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Takes 2 params, first is a color, second is a string
+# Does not print a new line
+function print {
+	printf "${1}${2}${Color_Off}"
+}
+
 if [ ! -f .42framework ]
 then
 	echo "You can only execute this script in a repo initialized with the 'init42' command."
@@ -16,36 +35,37 @@ else
 	dir="ex$EX_NB"
 fi
 
-echo "Compiling source files..."
+print $Purple "Testing $dir ========================================\n"
+print $Cyan "Compiling source files...\n"
 gcc -Wall -Wextra -Werror -o tests/test_$dir tests/test_$dir.c
 out=$?
 
 if [ "$out" -ne 0 ]
 then
 	echo $out
-	echo "Compilation unsuccessful. Exiting."
+	print $Red "Compilation unsuccessful. Exiting.\n"
 	exit 1
 fi
 
-echo "Compilation finished"
-echo "Running tests..."
+print $Green "Compilation finished\n"
+print $Cyan "Running tests...\n"
 
 ./tests/test_$dir
 out=$?
 
 if [ "$out" -ne 0 ]
 then
-	echo "Tests failed. Exiting"
+	print $Red "Tests failed. Exiting\n"
 	exit 1
 fi
 
-echo "Tests finished successfully."
+print $Green "Tests finished successfully.\n"
 
 if [ 0 -lt $# ] && [ "$1" = "--norm" ]
 then
 
-	echo "Checking norm..."
-
+	print $Cyan "Checking norm...\n"
 	norminette -R CheckForbiddenSourceHeader $dir 2> /dev/null
 fi
+print $Purple "=====================================================\n"
 exit 0
