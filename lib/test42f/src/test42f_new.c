0b8adff781f9eec23f121c890819cc50f49c1752
Adrien Lamit <alamit@e1z1r5p45.42.us.org>
2018-09-26 20:26:26 -0700

    added new array function

---
+++ lib/test42f/src/test42f_new.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test42f_new.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alamit <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/26 19:44:54 by alamit            #+#    #+#             */
+/*   Updated: 2018/09/26 20:24:36 by alamit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test42f.h"
+
+char	*test42f_new_str(char *str)
+{
+	char *res;
+	
+	res = malloc(sizeof(str));
+	return (strcpy(res, str));
+}
+
+int		*test42f_new_intarr(int size, ...)
+{
+	va_list args;
+	int		*res;
+	int		i;
+
+	res = malloc(size * sizeof(int));
+	va_start(args, size);
+	i = 0;
+	while (i < size)
+	{
+		*(res + i) = va_arg(args, int);
+		i++;
+	}
+	va_end(args);
+	return (res);
+}
