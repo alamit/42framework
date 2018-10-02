a3c30eb63f3abeffb15160de381a9cdd0d3f65cb
alamit <adrien@lamit.com>
2018-10-01 23:29:39 -0700

    IO test recording working

--- lib/test42f/include/test42f.h
+++ lib/test42f/include/test42f.h
@@ -6,7 +6,7 @@
 /*   By: alamit <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/26 12:19:40 by alamit            #+#    #+#             */
-/*   Updated: 2018/10/01 22:00:49 by alamit           ###   ########.fr       */
+/*   Updated: 2018/10/01 23:29:07 by alamit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,8 +62,9 @@ char	*test42f_intarr2str(size_t size, int *arr);
 bool	test42f_ptr_eq(char *title, void *ptr, void *expected);
 bool	test42f_ptr_neq(char *title, void *ptr, void *expected);
 
-int		test42f_io_record(char *tape);
-int		test42f_io_end_record();
+/* EXPERIMENTAL AND NON-PORTABLE */
+int		test42f_io_start_record();
+int		test42f_io_end_record(char **tape);
 
 /* New type functions returns dynamically allocated variables, they 
  ** should be freed.*/
--- lib/test42f/src/test42f_io.c
+++ lib/test42f/src/test42f_io.c
@@ -6,15 +6,15 @@
 /*   By: alamit <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/01 18:59:15 by alamit            #+#    #+#             */
-/*   Updated: 2018/10/01 22:05:03 by alamit           ###   ########.fr       */
+/*   Updated: 2018/10/01 23:28:24 by alamit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/test42f.h"
 
 int		_test42f_stdout_cpy = -1;
-
-int		test42f_io_record(char *recorder)
+char	*_test42f_io_tape = NULL;
+int		test42f_io_start_record()
 {
 	if (_test42f_stdout_cpy > -1)
 	{
@@ -22,14 +22,15 @@ int		test42f_io_record(char *recorder)
 				ending the previous record.");
 		return (1);
 	}
-	if (recorder != NULL)
-		return (1);
+	_test42f_io_tape = malloc(BUFSIZ);
+	fflush(stdout);
 	_test42f_stdout_cpy = dup(1);
-	setbuf(stdout, recorder);
+	freopen("NUL", "a", stdout);
+	setbuf(stdout, _test42f_io_tape);
 	return (0);
 }
 
-int		test42f_io_end_record(void)
+int		test42f_io_end_record(char **tape)
 {
 	if (_test42f_stdout_cpy == -1)
 	{
@@ -37,7 +38,11 @@ int		test42f_io_end_record(void)
 				test42f_io_record.");
 		return (1);
 	}
+	*tape = strdup(_test42f_io_tape);
+	freopen("NUL", "a", stdout);
 	dup2(_test42f_stdout_cpy, 1);
+	setbuf(stdout, NULL);
 	close(_test42f_stdout_cpy);
+	_test42f_io_tape = NULL;
 	return (0);
 }
